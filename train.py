b4a1056518444d588493c655791db863431c89e8
BeesyBumbleBee <sz.oleskiewicz@gmail.com>
2025-02-11 14:00:59 +0100

    Added parameters to modify network layers

--- train.py
+++ train.py
@@ -10,8 +10,14 @@ def main():
     args = parser.parse_args()
     training_data, test_data = load_data()
     netName = args.network_name
-    net = Network([784, 30, 10])
+    layers = ([784] + args.layers + [10])
+    net = Network(layers)
     net = load_network(netName, net)
+    if args.verbosity >= 2:
+        print(f"Initial biases: {net.biases}\n\n")
+        print(f"Initial weights: {net.weights}\n\n")
+    if args.verbosity >= 1:
+        print(f"Training network. Layers: {layers}")
     if args.testing is False:
         test_data = None
 
@@ -22,10 +28,12 @@ def main():
 def load_network(name: str, net: Network) -> Network:
     try:
         fp = gzip.open(f"{netpath}/{name}.pkl.gz", "rb")
+        print(f"Loading saved network: {name}");
         weights, biases = load(fp)
         fp.close()
         net.load_network(weights, biases)
     except FileNotFoundError:
+        print(f"Creating network: {name}");
         pass
     return net
 
@@ -42,4 +50,6 @@ if __name__ == "__main__":
     parser.add_argument("-b", "--batch-size", default=10, type=int)
     parser.add_argument("-n", "--network-name", default="digitreader")
     parser.add_argument("-t", "--testing", action="store_false")
+    parser.add_argument("-l", "--layers", nargs='+', type=int, default=[30])
+    parser.add_argument("-v", "--verbosity", action="count", default=0)
     main()
