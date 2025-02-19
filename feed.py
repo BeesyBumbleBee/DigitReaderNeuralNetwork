1585df04e2791a9b1735d826fe2bae4f12ce528e
BeesyBumbleBee <sz.oleskiewicz@gmail.com>
2025-02-19 11:46:22 +0100

    feed function accepts json files given -j argument

--- feed.py
+++ feed.py
@@ -1,6 +1,7 @@
 from network import load_network
 from time import perf_counter
 from mnist_loader import load_data
+from json import load
 import numpy as np
 import argparse
 
@@ -21,19 +22,29 @@ def textToData(txt: str):
             result.append(255)
     return np.asmatrix(result).transpose()
 
+def jsonToData(json_file):
+    input_data = load(json_file)
+    input_data = input_data['values']
+    input_data = np.asmatrix(input_data)
+    if input_data.shape[0] != 1:
+        return input_data
+    return input_data.transpose()
+
 def main():
     args = parser.parse_args()
     netName = args.network_name
     net = load_network(f"{netpath}/{netName}", None)
     try:
         with open(args.input_file, "r") as f:
-            input_data = textToData(f.read())
+            if (args.input_json):
+                input_data = jsonToData(f)
+            else:
+                input_data = textToData(f.read())
     except FileNotFoundError:
         print(f"File {args.input_file} not found")
         return 1
     
     display(input_data)
-    print(input_data)
     result = net.feedforward(input_data)
     for i, x in enumerate(result):
         print(f"{i} : {float(x.item(0)):3.2f}")
@@ -43,6 +54,7 @@ def main():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--network-name", action="store", default="digitreader")
+    parser.add_argument("-j", "--input-json", action="store_true");
     parser.add_argument("input_file")
 
     main()
